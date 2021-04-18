a6884866dc5fe1a2ae3a4f8cf192091c4f857033
Sinan Sarıçayır <sinansaricayir@gmail.com>
2021-04-18 18:59:28 +0300

    TemplateEngine

---
+++ app.js
@@ -0,0 +1,31 @@
+const path = require("path")
+const express = require('express')
+const exphbs  = require('express-handlebars')
+const app = express()
+const port = 3000
+const hostname = "127.0.0.1"
+
+app.use(express.static("public"))
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.get("/",(req,res)=>{
+  res.render("site/index")
+})
+
+app.get("/about", (req,res)=>{
+  res.render("site/about")
+})
+
+app.get("/blog", (req,res)=>{
+  res.render("site/blog")
+})
+
+
+
+
+app.listen(port,hostname, () => {
+  console.log(`Server Çalışıyor, http://${hostname}:${port}`)
+})
+
